0cbf6720bbe8f7459fdfa8a3b28fc8c16d2fc823
Tony Spencer <mobiletony@msn.com>
2019-10-15 10:31:35 -0600

    Added github

--- BlazingPizza.Web/Startup.cs
+++ BlazingPizza.Web/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using BlazingPizza.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +11,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -68,6 +72,44 @@ namespace BlazingPizza.Web
                     options.ClientId = Configuration["Authentication:Facebook:ClientId"];
                     options.ClientSecret = Configuration["Authentication:Facebook:ClientSecret"];
                     options.CallbackPath = "/ftoken";
+                })
+                .AddOAuth("github", options =>
+                {
+                    options.ClientId = Configuration["Authentication:Github:ClientId"];
+                    options.ClientSecret = Configuration["Authentication:Github:ClientSecret"];
+                    options.CallbackPath = "/gittoken";
+                    options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+                    options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+                    options.UserInformationEndpoint = "https://api.github.com/user";
+                    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+                    options.ClaimActions.MapJsonKey("urn:github:login", "login");
+                    options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
+                    options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
+                    options.Events = new OAuthEvents
+                    {
+                        OnCreatingTicket = async context =>
+                        {
+                            var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                            var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                            response.EnsureSuccessStatusCode();
+
+                            var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+                            
+                            var nameToken = user.SelectToken("name");
+                            // no name value, use login instead
+                            if (string.IsNullOrEmpty(nameToken.ToString()))
+                            {
+                                var jprop = nameToken.Parent as JProperty;
+                                jprop.Value = user.Value<string>("login");
+                            }
+                            
+                            context.RunClaimActions(user);
+                        }
+                    };
                 })
                 .AddTwitter("twitter",twitterOptions =>
                 {
